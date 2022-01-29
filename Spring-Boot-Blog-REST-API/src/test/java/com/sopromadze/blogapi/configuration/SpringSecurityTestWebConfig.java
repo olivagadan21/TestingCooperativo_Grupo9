51e3c45290499e92a10e91923672ed60fc48e7fb
Jaime <jaimejimenezrios10@gmail.com>
2022-01-29 20:55:42 +0100

    Añadidos algunos controladores

---
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/configuration/SpringSecurityTestWebConfig.java
@@ -0,0 +1,57 @@
+package com.sopromadze.blogapi.configuration;
+
+import com.sopromadze.blogapi.model.role.Role;
+import com.sopromadze.blogapi.model.role.RoleName;
+import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.UserProfile;
+import com.sopromadze.blogapi.security.UserPrincipal;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserCache;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.*;
+
+@TestConfiguration
+public class SpringSecurityTestWebConfig {
+
+    @Bean("customUserDetailsService")
+    @Primary
+    public UserDetailsService userDetailsService() {
+
+        Role rol1 = new Role();
+        rol1.setName(RoleName.ROLE_USER);
+
+        Role rol2 = new Role();
+        rol2.setName(RoleName.ROLE_ADMIN);
+
+        List<Role> rol = Arrays.asList(rol1);
+
+        List<Role> ambosRoles = Arrays.asList(rol1,rol2);
+
+        User admin = new User();
+        admin.setPassword("admin");
+        admin.setUsername("admin");
+        admin.setRoles(ambosRoles);
+
+
+
+
+        User user = new User();
+        user.setPassword("user");
+        user.setUsername("user");
+        user.setRoles(rol);
+
+        UserPrincipal userP = UserPrincipal.create(user);
+        UserPrincipal adminP = UserPrincipal.create(admin);
+
+        return new InMemoryUserDetailsManager(List.of(adminP, userP));
+
+
+    }
+
+
+}
---
+++ Spring-Boot-Blog-REST-API/src/test/java/com/sopromadze/blogapi/controller/PostControllerTest.java
@@ -0,0 +1,205 @@
+package com.sopromadze.blogapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sopromadze.blogapi.configuration.SpringSecurityTestWebConfig;
+import com.sopromadze.blogapi.model.Category;
+import com.sopromadze.blogapi.model.Post;
+import com.sopromadze.blogapi.model.Tag;
+import com.sopromadze.blogapi.model.role.Role;
+import com.sopromadze.blogapi.model.role.RoleName;
+import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.ApiResponse;
+import com.sopromadze.blogapi.payload.PostRequest;
+import com.sopromadze.blogapi.payload.PostResponse;
+import com.sopromadze.blogapi.security.UserPrincipal;
+import com.sopromadze.blogapi.service.PostService;
+import com.sopromadze.blogapi.service.impl.PhotoServiceImpl;
+import com.sopromadze.blogapi.service.impl.PostServiceImpl;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@Log
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {SpringSecurityTestWebConfig.class}, properties = {"spring.main.allow-bean-definition-overriding=true"})
+public class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    PostServiceImpl postServiceImpl;
+
+
+    private UserPrincipal userPrincipal;
+    private UserPrincipal userPrincipal2;
+    private User user;
+    private User user2;
+    private Role rol;
+    private Role rol2;
+    private ApiResponse apiResponse;
+    private Post post1;
+    private PostRequest postRequest;
+    private Post post2;
+    private Category category;
+    private Tag tag;
+    private PostResponse postResponse;
+
+    @BeforeEach
+    void initData() {
+
+        rol = new Role();
+        rol.setName(RoleName.ROLE_ADMIN);
+
+        List<Role> roles = Arrays.asList(rol);
+
+        rol2 = new Role();
+        rol2.setName(RoleName.ROLE_USER);
+
+        List<Role> roles2 = Arrays.asList(rol2);
+
+        post1 = new Post();
+        post1.setId(1L);
+        post1.setTitle("Post buenardo");
+        post1.setCreatedAt(Instant.now());
+        post1.setUpdatedAt(Instant.now());
+
+        user = new User();
+        user.setId(3L);
+        user.setRoles(roles);
+        user.setPosts(List.of(post1));
+
+        user2 = new User();
+        user2.setId(4L);
+        user2.setRoles(roles2);
+        user2.setPosts(List.of(post1));
+
+        userPrincipal = UserPrincipal.builder()
+                .id(user.getId())
+                .authorities(user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
+                .build();
+
+        userPrincipal2 = UserPrincipal.builder()
+                .id(user2.getId())
+                .authorities(user2.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList()))
+                .build();
+
+
+        apiResponse = new ApiResponse();
+        apiResponse.setSuccess(true);
+        apiResponse.setMessage("You successfully deleted post");
+
+
+        category = new Category();
+        category.setId(2L);
+        category.setCreatedBy(user.getId());
+        category.setName("Categoría chula");
+
+        tag = new Tag();
+        tag.setName("Tag bueno");
+
+
+        List<String> tags = Arrays.asList(tag.getName());
+
+        List<Tag> tags2 = Arrays.asList(tag);
+
+        postRequest = new PostRequest();
+        postRequest.setCategoryId(category.getId());
+        postRequest.setTitle("Post Buenardo");
+        postRequest.setBody("Post muy bueno, tan bueno que lo pongo aqui porque me gusta mucho");
+        postRequest.setTags(tags);
+
+        post2 = new Post();
+        post2.setBody(postRequest.getBody());
+        post2.setTitle(postRequest.getTitle());
+        post2.setBody(postRequest.getBody());
+        post2.setCategory(category);
+        post2.setUser(user);
+        post2.setTags(tags2);
+
+        postResponse = new PostResponse();
+
+        postResponse.setTitle(post2.getTitle());
+        postResponse.setBody(post2.getBody());
+        postResponse.setCategory(post2.getCategory().getName());
+        postResponse.setTags(tags);
+
+
+
+    }
+
+    @WithMockUser(authorities = {"ROLE_USER", "ROLE_ADMIN"})
+    @Test
+    void whenDeletePost_returns200() throws Exception {
+
+        System.out.println(postServiceImpl);
+
+        when(postServiceImpl.deletePost(post1.getId(), userPrincipal)).thenReturn(apiResponse);
+        mockMvc.perform(delete("/api/posts/{id}", post1.getId())
+                        .contentType("application/json"))
+                .andExpect(status().isOk()).andDo(print());
+
+    }
+
+
+    @Test
+    void whenDeletePost_returns401() throws Exception {
+
+        System.out.println(postServiceImpl);
+
+        when(postServiceImpl.deletePost(post1.getId(), userPrincipal)).thenReturn(apiResponse);
+        mockMvc.perform(delete("/api/posts/{id}", post1.getId())
+                        .contentType("application/json"))
+                .andExpect(status().isUnauthorized()).andDo(print());
+
+    }
+
+    @WithMockUser(authorities = {"ROLE_USER"})
+    @Test
+    void whenAddPost_returns200() throws Exception {
+
+        when(postServiceImpl.addPost(postRequest, userPrincipal)).thenReturn(postResponse);
+
+        mockMvc.perform(post("/api/posts")
+                .content(objectMapper.writeValueAsString(postRequest))
+                .contentType("application/json"))
+                .andExpect(status().isCreated()).andDo(print());
+
+    }
+
+
+    @Test
+    void whenAddPost_returns401() throws Exception{
+
+        when(postServiceImpl.addPost(postRequest, userPrincipal)).thenReturn(postResponse);
+
+        mockMvc.perform(post("/api/posts")
+                        .content(objectMapper.writeValueAsString(postRequest))
+                        .contentType("application/json"))
+                .andExpect(status().isUnauthorized()).andDo(print());
+    }
+
+}
